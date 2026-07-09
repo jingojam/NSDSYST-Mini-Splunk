c1f84cd108a34d51f68bd41102d9ab8582ccea51
jingojam <xtian.arano1224@gmail.com>
2026-07-09 18:05:01 +0800

    repo cleanup

--- server/build.sh
+++
@@ -1,2 +0,0 @@
-protoc -I=. --cpp_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` mini_splunk_protobuf.proto
-g++ main.cpp mini_splunk_protobuf.pb.cc mini_splunk_protobuf.grpc.pb.cc WorkerNode.cpp -o worker.exe     -lgrpc++     -lprotobuf     -lsqlite3     -lgrpc
